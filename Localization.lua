2750d57ff34fd50522546b0cd5343a1e568fad7b
Jonas Mueller <jonas@muellers.one>
2017-01-05 15:35:21 +0100

    Fix indent and remove redundant localisations

--- Localization.lua
+++ Localization.lua
@@ -1,20 +1,12 @@
 --[[
 	Prot - English Localization
 
-	By: Danjo
+	By: Ollowain
 ]]--
 
 BINDING_NAME_PROT			= "Attack";
 BINDING_HEADER_PROT			= "Prot";
 
-CLASS_DRUID_PROT			= "Druid";
-CLASS_HUNTER_PROT			= "Hunter";
-CLASS_MAGE_PROT				= "Mage";
-CLASS_PALADIN_PROT			= "Paladin";
-CLASS_PRIEST_PROT			= "Priest";
-CLASS_ROGUE_PROT			= "Rogue";
-CLASS_SHAMAN_PROT			= "Shaman";
-CLASS_WARLOCK_PROT			= "Warlock";
 CLASS_WARRIOR_PROT			= "Warrior";
 
 ABILITY_BATTLE_SHOUT_PROT		= "Battle Shout";
@@ -24,10 +16,6 @@ ABILITY_BERSERKER_STANCE_PROT		= "Berserker Stance";
 ABILITY_BLOODRAGE_PROT			= "Bloodrage";
 ABILITY_BLOODTHIRST_PROT		= "Bloodthirst";
 ABILITY_CHALLENGING_SHOUT_PROT	= "Challenging Shout";
-ABILITY_CHARGE_PROT			= "Charge";
-ABILITY_CLEAVE_PROT			= "Cleave";
---ABILITY_CONCUSSION_BLOW_PROT		= "Concussion Blow";
-ABILITY_DEATH_WISH_PROT		= "Death Wish";
 ABILITY_DEFENSIVE_STANCE_PROT		= "Defensive Stance";
 ABILITY_DEMORALIZING_SHOUT_PROT		= "Demoralizing Shout";
 ABILITY_DISARM_PROT			= "Disarm";
@@ -38,85 +26,19 @@ ABILITY_INTERCEPT_PROT			= "Intercept";
 ABILITY_INTIMIDATING_SHOUT_PROT	= "Intimidating Shout";
 ABILITY_LAST_STAND_PROT		= "Last Stand";
 ABILITY_MOCKING_BLOW_PROT		= "Mocking Blow";
-ABILITY_MORTAL_STRIKE_PROT		= "Mortal Strike";
 ABILITY_OVERPOWER_PROT			= "Overpower";
---ABILITY_PIERCING_HOWL_PROT		= "Piercing Howl";
-ABILITY_PUMMEL_PROT			= "Pummel";
---ABILITY_RECKLESSNESS_PROT		= "Recklessness";
-ABILITY_REND_PROT			= "Rend";
---ABILITY_RETALIATION_PROT		= "Retaliation";
 ABILITY_REVENGE_PROT			= "Revenge";
 ABILITY_SHIELD_BASH_PROT		= "Shield Bash";
 ABILITY_SHIELD_BLOCK_PROT		= "Shield Block";
 ABILITY_SHIELD_SLAM_PROT		= "Shield Slam";
 ABILITY_SHIELD_WALL_PROT		= "Shield Wall";
---ABILITY_SLAM_PROT			= "Slam";
 ABILITY_SUNDER_ARMOR_PROT		= "Sunder Armor";
 ABILITY_SWEEPING_STRIKES_PROT		= "Sweeping Strikes";
 ABILITY_TAUNT_PROT			= "Taunt";
 ABILITY_THUNDER_CLAP_PROT		= "Thunder Clap";
-ABILITY_WHIRLWIND_PROT			= "Whirlwind";
 
-RACIAL_BERSERKING_PROT			= "Berserking";
-
-CHAT_OVERPOWER1_PROT			= "You attack.(.+) dodges.";
-CHAT_OVERPOWER2_PROT			= "Your (.+) was dodged by (.+).";
-CHAT_OVERPOWER3_PROT			= "Your Overpower crits (.+) for (%d+)."
-CHAT_OVERPOWER4_PROT			= "Your Overpower hits (.+) for (%d+)."
-CHAT_OVERPOWER5_PROT			= "Your Overpower missed (.+)."
-CHAT_RUNNER_PROT			= "%s attempts to run away in fear!"
-CHAT_CAST_PROT				= "(.+) begins to cast (.+).";
-CHAT_INTERRUPT1_PROT			= "You interrupt (.+).";
-CHAT_INTERRUPT2_PROT			= "Your Pummel was (.+) by (.+).";
-CHAT_INTERRUPT3_PROT			= "Your Shield Bash was (.+) by (.+).";
-CHAT_INTERRUPT4_PROT			= "Your Pummel missed (.+)."
-CHAT_INTERRUPT5_PROT			= "Your Shield Bash missed (.+)."
-
-CHAT_FEAR_PROT				= "You are afflicted by Fear.";
-CHAT_INTIMIDATING_SHOUT_PROT		= "You are afflicted by Intimidating Shout.";
-CHAT_PSYCHIC_SCREAM_PROT		= "You are afflicted by Psychic Scream.";
-CHAT_PANIC_PROT				= "You are afflicted by Panic.";
-CHAT_BELLOWING_ROAR_PROT		= "You are afflicted by Bellowing Roar.";
-CHAT_ANCIENT_DESPAIR_PROT		= "You are afflicted by Ancient Despair.";
-CHAT_TERRIFYING_SCREECH_PROT		= "You are afflicted by Terrifying Screech.";
-CHAT_HOWL_OF_TERROR_PROT		= "You are afflicted by Howl of Terror.";
-CHAT_SAP_PROT				= "You are afflicted by Sap.";
-CHAT_GOUGE_PROT				= "You are afflicted by Gouge.";
-CHAT_REPENTANCE_PROT			= "You are afflicted by Repentance.";
-CHAT_ROCKET_HELM_PROT			= "You are afflicted by Reckless Charge.";
-
-CHAT_FEAR2_PROT				= "Fear fades from you.";
-CHAT_INTIMIDATING_SHOUT2_PROT		= "Intimidating Shout fades from you.";
-CHAT_PSYCHIC_SCREAM2_PROT		= "Psychic Scream fades from you.";
-CHAT_PANIC2_PROT			= "Panic fades from you.";
-CHAT_BELLOWING_ROAR2_PROT		= "Bellowing Roar fades from you.";
-CHAT_ANCIENT_DESPAIR2_PROT		= "Ancient Despair fades from you.";
-CHAT_TERRIFYING_SCREECH2_PROT		= "Terrifying Screech fades from you.";
-CHAT_HOWL_OF_TERROR2_PROT		= "Howl of Terror fades from you.";
-CHAT_SAP2_PROT				= "Sap fades from you.";
-CHAT_GOUGE2_PROT			= "Gouge fades from you.";
-CHAT_REPENTANCE2_PROT			= "Repentance fades from you.";
-CHAT_ROCKET_HELM2_PROT			= "Reckless Charge fades from you.";
-
-ITEM_GAUNTLETS1_PROT			= "Knight-Lieutenant's Plate Gauntlets";
-ITEM_GAUNTLETS2_PROT			= "Marshal's Plate Gauntlets";
-ITEM_GAUNTLETS3_PROT			= "Blood Guard's Plate Gloves";
-ITEM_GAUNTLETS4_PROT			= "General's Plate Gauntlets";
-ITEM_SHIELDS_PROT			= "Shields";
-
-SLASH_PROT_ENABLED			= "enabled";
-SLASH_PROT_DISABLED			= "disabled";
-SLASH_PROT_DEFAULT			= "default";
-SLASH_PROT_DEBUG			= "Debug";
-SLASH_PROT_AUTOATTACK			= "Autoattack";
-SLASH_PROT_DANCE			= "Rage allowed to be wasted when switching stance set to ";
-SLASH_PROT_RAGE				= "Maximum rage allowed when using abilities to gain rage set to ";
-SLASH_PROT_RAGE				= "Minimum rage required when using Heroic Strike or Cleave set to ";
-SLASH_PROT_BLOODRAGE			= "Minimum percent of health required when using Bloodrage set to ";
-SLASH_PROT_HAMSTRING			= "Maximum percent of health allowed when using Hamstring on NPCs set to ";
-SLASH_PROT_LOWTHREAT			= "Low threat enabled, enabled Cleave.";
-SLASH_PROT_HIGHTHREAT			= "High threat enabled, enabled Heroic Strike.";
-SLASH_PROT_TROLL			= "Maximum percent of health allowed when using Berserking set to ";
-SLASH_PROT_STANCE			= "Primary stance set to ";
-SLASH_PROT_NOSTANCE			= "Stance switching ";
-SLASH_PROT_HELP				= "Help text, under construction!";
+SLASH_PROT_ENABLED    = "enabled";
+SLASH_PROT_DISABLED   = "disabled";
+SLASH_PROT_DEFAULT    = "default";
+SLASH_PROT_DEBUG      = "Debug";
+SLASH_PROT_HELP       = "Help text, under construction!";
